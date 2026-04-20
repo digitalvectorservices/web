9dfb0feea3cc37e54b85ff673aa76391a31d55e5
Vector <digitalvectorservicios@gmail.com>
2026-04-20 16:16:03 -0300

    servicios

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
 import { WhatsAppButton } from "./components/Whatsapp";
+import { Services } from "./pages/Services";
 
 export default function App() {
   return (
     <div className="bg-black text-white font-sans">
       <Header />
-      <Home />
+      {/* <Home /> */}
+      <Services/>
       <Footer />
       <WhatsAppButton />
     </div>
